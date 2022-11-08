e9990fda513a3fd14171009fcd9d8928b6607520
Nick Ganter <nickganter91@gmail.com>
2022-11-07 20:53:25 -0600

    Added more validation on password on serverside models.

--- RecipeCrawler.Entities/Models/CreateAccountModel.cs
+++ RecipeCrawler.Entities/Models/CreateAccountModel.cs
@@ -6,7 +6,7 @@ namespace RecipeCrawler.Entities.Models
     {
         [Required, EmailAddress]
         public string Email { get; set; }
-        [Required, MinLength(8, ErrorMessage = "Username must be at least 8 characters long")]
+        [Required, MaxLength(256, ErrorMessage = "Username cannot exceed 256 characters."), MinLength(8, ErrorMessage = "Username must be at least 8 characters long.")]
         public string Username { get; set; }
         [Required, RegularExpression(@"^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$", ErrorMessage = "Password must be at least 6 characters and contain one letter, number, and special symbol.")]
         public string Password { get; set; }
