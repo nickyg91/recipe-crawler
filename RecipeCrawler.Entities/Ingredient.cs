078d6e7062e02c56825ca7b75541770af3bc8cb5
Nick Ganter <nickganter91@gmail.com>
2023-02-19 21:11:40 -0600

    Fixed package issues. Added more frontend models.

--- RecipeCrawler.Entities/Ingredient.cs
+++ RecipeCrawler.Entities/Ingredient.cs
@@ -2,7 +2,7 @@
 {
     public class Ingredient : BaseEntity
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public MeasurementsEnum Measurement { get; set; }
         public int RecipeId { get; set; }
         public Recipe? Recipe { get; set; }
--- RecipeCrawler.Web/recipe-crawler/src/models/shared/recipe.model.ts
+++ RecipeCrawler.Web/recipe-crawler/src/models/shared/recipe.model.ts
@@ -1,5 +1,10 @@
+import { Ingredient } from "./ingredient.model";
+import { Step } from "./step.model";
+
 export class Recipe {
   name!: string;
   crawledHtml?: string | null;
   cookbookId!: number;
+  steps?: Step[] | null;
+  ingredients?: Ingredient[] | null;
 }
