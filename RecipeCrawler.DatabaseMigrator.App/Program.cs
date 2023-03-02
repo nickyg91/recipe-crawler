62b3b91c22d188f2c50a4a2c40365bc1d49e89b7
Nick Ganter <nickganter91@gmail.com>
2023-03-02 17:11:35 -0600

    added new migrator app to run db migrations

---
+++ RecipeCrawler.DatabaseMigrator.App/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile($"");
+
+configuration.Build();
+    
