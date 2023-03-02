aa3fdb676dc812999815a51ff14f70c516e649d9
Nick Ganter <nickganter91@gmail.com>
2023-03-01 21:42:57 -0600

    Trying to fix ef properties in recipe to no avail.

--- RecipeCrawler.DatabaseMigrator/ChefferDbContextDesignTimeFactory.cs
+++ RecipeCrawler.DatabaseMigrator/ChefferDbContextDesignTimeFactory.cs
@@ -10,15 +10,13 @@ namespace RecipeCrawler.DatabaseMigrator
         public ChefferDbContext CreateDbContext(string[] args)
         {
             var builder = new ConfigurationBuilder()
-                       .AddJsonFile($"appsettings.json", false)
-                       .AddJsonFile($"appsettings.Development.json", false)
                        .AddEnvironmentVariables();
             var configuration = builder.Build();
             var connectionString = configuration.GetConnectionString("cheffer");
             DbContextOptionsBuilder<ChefferDbContext> optionsBuilder = new DbContextOptionsBuilder<ChefferDbContext>()
-                .UseNpgsql(connectionString, builder =>
+                .UseNpgsql(connectionString, npgsqlBuilder =>
                 {
-                    builder.MigrationsAssembly("RecipeCrawler.DatabaseMigrator");
+                    npgsqlBuilder.MigrationsAssembly("RecipeCrawler.DatabaseMigrator");
                 });
             return new ChefferDbContext(optionsBuilder.Options);
         }
