a0f7be17f053af98cea2a0ea71f77f152cb65041
Nick Ganter <nickganter91@gmail.com>
2023-03-02 21:24:46 -0600

    Finished implementing automapper profiles for existing viewmodels

--- RecipeCrawler.ViewModels/Mappers/IngredientProfile.cs
+++ RecipeCrawler.ViewModels/Mappers/IngredientProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using RecipeCrawler.Entities;
+using RecipeCrawler.ViewModels.ViewModels;
 
 namespace RecipeCrawler.ViewModels.Mappers;
 
@@ -6,6 +8,13 @@ public class IngredientProfile : Profile
 {
     public IngredientProfile()
     {
-        
+        CreateMap<IngredientViewModel, Ingredient>(MemberList.Destination)
+            .ForMember(dst => dst.Id, x => x.MapFrom(src => src.Id))
+            .ForMember(dst => dst.Measurement, x => x.MapFrom(src => src.Measurement))
+            .ForMember(dst => dst.Name, x => x.MapFrom(src => src.Name))
+            .ForMember(dst => dst.RecipeId, x => x.MapFrom(src => src.RecipeId))
+            .ForMember(dst => dst.Recipe, x => x.Ignore())
+            .ForMember(dst => dst.CreatedAtUtc, x => x.Ignore())
+            .ReverseMap();
     }
 }
